1f10a711d0c59fbe670e3b3b6f6dca8cab0c6b5a
maxshow1970 <sashamaks70@gmail.com>
2018-02-14 09:26:22 +0200

    Finish_end

--- src/store.js
+++ src/store.js
@@ -1,5 +1,9 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import rootReducer from "./reducers";
 import thunk from "redux-thunk";
 
 export default createStore(rootReducer, applyMiddleware(thunk));
+
+
+
+
